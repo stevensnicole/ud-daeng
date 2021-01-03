472c1687ee2e986e96e0f43fbb95eb8536be5158
Nicole Stevens <nicole.stevens@gmail.com>
2021-01-03 22:32:42 +0000

    functions added for loading staging tables

--- data-warehouse/etl.py
+++ data-warehouse/etl.py
@@ -1,6 +1,6 @@
 import configparser
 import psycopg2
-from sql_queries import copy_table_queries, insert_table_queries
+from sql_queries import copy_table_queries#, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
@@ -9,10 +9,10 @@ def load_staging_tables(cur, conn):
         conn.commit()
 
 
-def insert_tables(cur, conn):
-    for query in insert_table_queries:
-        cur.execute(query)
-        conn.commit()
+#def insert_tables(cur, conn):
+#    for query in insert_table_queries:
+#        cur.execute(query)
+#        conn.commit()
 
 
 def main():
--- data-warehouse/sql_queries.py
+++ data-warehouse/sql_queries.py
@@ -7,7 +7,7 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
 songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
 user_table_drop = "DROP TABLE IF EXISTS users;"
@@ -18,122 +18,124 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 # CREATE TABLES
 
 staging_events_table_create= ("""CREATE TABLE IF NOT EXISTS staging_events (
-                                artist text,
-                                auth text,
-                                first_name text,
-                                gender text,
-                                item_in_session integer,
-                                last_name  text,
-                                length numeric,
-                                level text,
-                                location text,
-                                method text,
-                                page text,
-                                registration numeric,
-                                session_id integer,
-                                song text,
-                                status integer,
-                                ts bigint,
-                                user_agent text,
-                                user_id integer
+                                artist TEXT,
+                                auth TEXT,
+                                first_name TEXT,
+                                gender TEXT,
+                                item_in_session INTEGER,
+                                last_name  TEXT,
+                                length NUMERIC,
+                                level TEXT,
+                                location TEXT,
+                                method TEXT,
+                                page TEXT,
+                                registration NUMERIC,
+                                session_id INTEGER,
+                                song TEXT,
+                                status INTEGER,
+                                ts BIGINT,
+                                user_agent TEXT,
+                                user_id INTEGER
                                 );
 """)
 
 staging_songs_table_create = ("""CREATE TABLE IF NOT EXISTS staging_songs (
-                                num_songs integer,
-                                artist_id text,
-                                artist_latitude numeric,
-                                artist_longitude numeric,
-                                artist_location text,
-                                artist_name text,
-                                song_id text,
-                                title text,
-                                duration numeric,
-                                year integer
+                                num_songs INTEGER,
+                                artist_id TEXT,
+                                artist_latitude NUMERIC,
+                                artist_longitude NUMERIC,
+                                artist_location TEXT,
+                                artist_name TEXT,
+                                song_id TEXT,
+                                title TEXT,
+                                duration NUMERIC,
+                                year INTEGER
                                 );
 """)
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays ( 
-                            songplay_id integer identity(1,1) primary key, 
-                            start_time timestamp not null, 
-                            user_id int not null,
-                            level text not null,
-                            song_id text not null,
-                            artist_id text not null,
-                            session_id integer not null,
-                            location text not null,
-                            user_agent text not null
+                            songplay_id INTEGER identity(1,1) PRIMARY KEY, 
+                            start_time timestamp NOT NULL, 
+                            user_id int NOT NULL,
+                            level TEXT NOT NULL,
+                            song_id TEXT NOT NULL,
+                            artist_id TEXT NOT NULL,
+                            session_id INTEGER NOT NULL,
+                            location TEXT NOT NULL,
+                            user_agent TEXT NOT NULL
                             ); 
                         """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users ( 
-                        user_id int PRIMARY KEY not null, 
-                        first_name text not null,
-                        last_name text not null,
-                        gender text not null,
-                        level text not null
+                        user_id int PRIMARY KEY, 
+                        first_name TEXT NOT NULL,
+                        last_name TEXT NOT NULL,
+                        gender TEXT NOT NULL,
+                        level TEXT NOT NULL
                         ); 
                     """)
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs ( 
-                        song_id text primary key not null, 
-                        title text not null,
-                        artist_id text not null,
-                        year int not null,
-                        duration float not null,
+                        song_id TEXT PRIMARY KEY NOT NULL, 
+                        title TEXT NOT NULL,
+                        artist_id TEXT NOT NULL,
+                        year int NOT NULL,
+                        duration FLOAT NOT NULL
                         ); 
                     """)
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists ( 
-                            artist_id text PRIMARY KEY NOT NULL,
-                            name text NOT NULL,
-                            location text NOT NULL, 
-                            latitude float NOT NULL, 
-                            longitude float NOT NULL
+                            artist_id TEXT PRIMARY KEY,
+                            name TEXT NOT NULL,
+                            location TEXT NOT NULL, 
+                            latitude FLOAT NOT NULL, 
+                            longitude FLOAT NOT NULL
                             ); 
                     """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
-                        start_time timestamp PRIMARY KEY NOT NULL,
-                        hour integer NOT NULL,
-                        day integer not null,
-                        week integer not null,
-                        month integer not null,
-                        year integer not null,
-                        weekday integer not null
+                        start_time timestamp PRIMARY KEY,
+                        hour INTEGER NOT NULL,
+                        day INTEGER NOT NULL,
+                        week INTEGER NOT NULL,
+                        month INTEGER NOT NULL,
+                        year INTEGER NOT NULL,
+                        weekday INTEGER NOT NULL
                         ); 
                     """)
 
 # STAGING TABLES
 
-staging_events_copy = ("""copy staging_events from 's3://udacity-labs/tickets/full/full.csv.gz'
+staging_events_copy = ("""copy staging_events from 's3://udacity-dend/log_data'
     credentials 'aws_iam_role={}'
-    gzip delimiter ';' compupdate off region 'us-west-2';
-""".format(DWH_ROLE_ARN)
+    json 's3://udacity-dend/log_json_path.json' compupdate off region 'us-west-2';
+""").format(config.get("IAM_ROLE", "ARN"))
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = ("""copy staging_songs from 's3://udacity-dend/song_data'
+    credentials 'aws_iam_role={}'
+    json 'auto' compupdate off region 'us-west-2';
+""").format(config.get("IAM_ROLE", "ARN"))
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
-""")
+#songplay_table_insert = ("""
+#""")
 
-user_table_insert = ("""
-""")
+#user_table_insert = ("""
+#""")
 
-song_table_insert = ("""
-""")
+#song_table_insert = ("""
+#""")
 
-artist_table_insert = ("""
-""")
+#artist_table_insert = ("""
+#""")
 
-time_table_insert = ("""
-""")
+#time_table_insert = ("""
+#""")
 
 # QUERY LISTS
 
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+copy_table_queries = [staging_events_copy,staging_songs_copy]
+#insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
